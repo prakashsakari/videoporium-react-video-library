fe0e96edba6b8afb099d8fd185345dd4ce125120
Prakash Sakari <sakariprakash@gmail.com>
2022-05-04 08:42:13 +0530

    refactored go to home from watch later

--- src/pages/WatchLater/WatchLater.js
+++ src/pages/WatchLater/WatchLater.js
@@ -1,20 +1,34 @@
+import { useNavigate } from "react-router-dom";
 import { Navbar, SideBar, LikedVideo } from "../../components";
 import { usePlaylist } from "../../context";
+import "../Playlist.css";
 
 export const WatchLater = () => {
   const { watchLater } = usePlaylist();
-
+  const navigate = useNavigate();
+  
   return (
         <>
         <Navbar />
         <div class="d-flex gap mg">
             <SideBar />
             <main class="main-video-container scrollable-element">
-            {watchLater && watchLater.length > 0 ? (
-                watchLater.map((video) => <LikedVideo video={video} />)
-            ) : (
-                <h2>Nothing to display</h2>
-            )}
+                <h2 className="heading-2 page-title title-width">Watch Later</h2>
+                {watchLater && watchLater.length > 0 ? (
+                    watchLater.map((video) => <LikedVideo video={video} />)
+                ) : (
+                    <div className="notify-message">
+                <h3 className="heading-3">
+                    You have not saved any video to watch later yet.{" "}
+                    <span
+                    className="back-home cursor"
+                    onClick={() => navigate("/")}
+                    >
+                    Explore all videos
+                    </span>
+                </h3>
+                </div>
+                )}
             </main>
         </div>
         </>
